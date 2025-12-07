791798a378c4655ea84899ae913edf0e629c422a
rgap <r.guzmanap@gmail.com>
2025-12-07 13:51:27 -0500

    feat(F1-T01): adjust initial setup and clean unused ui components

--- src/pages/components.tsx
+++ src/pages/components.tsx
@@ -1,11 +1,4 @@
-import { notFound } from "next/navigation";
-export const dynamic = "force-dynamic";
-
 import "../app/globals.css";
-
-import { Button } from "@/components/ui/button";
-import { ThemeProvider } from "@/components/theme-provider";
-import { ModeToggle } from "@/components/ui/mode-toggle";
 
 export async function getServerSideProps() {
   if (process.env.NODE_ENV !== "development") {
@@ -17,25 +10,8 @@ export async function getServerSideProps() {
 // components demo page
 export default function ComponentsDemoPage() {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <div className="flex flex-col items-center">
-        {/* button */}
-        <div className="flex flex-col items-center justify-center">
-          <h1>Button</h1>
-          <Button>Click me</Button>
-        </div>
-
-        {/* mode toggle */}
-        <div>
-          <h1>Mode Toggle</h1>
-          <ModeToggle />
-        </div>
-      </div>
-    </ThemeProvider>
+    <div className="flex flex-col items-center">
+      Here goes the components demo page.
+    </div>
   );
 }
